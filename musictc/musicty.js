c66344443a151c83789d9157dc0269a9bae2488a
1426239465 <63838224+1426239465@users.noreply.github.com>
2021-11-12 22:27:10 +0800

    Add files via upload

---
+++ musictc/musicty.js
@@ -0,0 +1,29 @@
+function playMusic() {
+        var audioEle = document.getElementById("audio");
+      if (audioEle.paused){
+        audioEle.play();
+      }else {
+        audioEle.pause();
+      }
+    }
+    
+    $("#music").click(function(){
+      play();
+    });
+
+    var player = document.getElementById("audio");
+    play();
+    function play(){
+    swal("请问是否开启音乐🎶一边阅读一边听歌呢❓\n" ,{
+            buttons: {
+              allowcancel: "开启音乐",
+                allow: "关闭音乐"
+            }
+        }).then(function(value) {
+            if (value == "allow") {
+                player.pause()
+            } else {
+                player.play();
+            }
+        });
+    }
